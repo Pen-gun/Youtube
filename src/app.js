f5865452d89878b30e1b07ea02c1cf39c9619865
Paras Limbu <paras078@academiacollege.edu.np>
2025-11-30 13:46:57 +0545

    feat: implement user registration route and enhance request size limits

--- src/app.js
+++ src/app.js
@@ -10,14 +10,20 @@ app.use(cors({
 }))
 app.use(express.json(
     {
-        limit: '16kb',
+        limit: '10mb',
     }
 ))
 app.use(express.urlencoded({
     extended: true,
-    limit: '16kb',
+    limit: '10mb',
 }))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+//route import
+import userRouter from './routes/user.routes.js';
+
+//route declaration
+app.use('/api/v1/users', userRouter);
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import app from './app.js';
 dotenv.config({path: './.env'});
 
 connectToDB().then(()=>{
-    app.listen(process.env.PORT|| 8080, ()=>{
+    app.listen(process.env.PORT, ()=>{
         console.log(`Server is running on port ${process.env.PORT}`);
     });
     app.on('error', (err)=>{
