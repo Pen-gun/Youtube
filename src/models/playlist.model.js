928a603a4e5e90944e62c1b0f2929461d36bdee2
Paras Limbu <paras078@academiacollege.edu.np>
2025-12-07 16:50:35 +0545

    feat: add models for Comment, Like, Playlist, and Tweet with schema definitions

---
+++ src/models/playlist.model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from 'mongoose';
+
+const playlistSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    videos: {
+        type: [Schema.Types.ObjectId],
+        ref: "Video",
+        default: []
+    }
+},{timestamps: true});
+
+export const Playlist = mongoose.model('Playlist', playlistSchema);
